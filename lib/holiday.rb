29bca89bb0091872956eea8c199eb3eb3d76feb1
Jason Vance <jbvance@gmail.com>
2017-11-24 16:57:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash.each do |season, holiday|
       str += "#{season}:\n  "
       holiday.each do |name, array|
-        str += "#{holiday}: #{array}"
+        str += "#{name}: #{array}"
       end
     end
     binding.pry
