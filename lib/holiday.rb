3bb886ed9657db145ac5ca8e1d39c5ecab38a8ec
Jason Vance <jbvance@gmail.com>
2017-11-24 16:55:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash.each do |season, holiday|
       str += "#{season.capitalize}:\n  "
       holiday.each do |name, array|
-        string += holiday.capitalize + ": " + array
+        string += "#{holiday.capitalize}: #{array}""
       end
     end
     binding.pry
